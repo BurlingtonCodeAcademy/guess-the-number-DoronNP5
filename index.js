5f7d8419702ecb8e88d321203926e55d58eb93dc
DoronNP5 <54821310+DoronNP5@users.noreply.github.com>
2019-10-08 19:32:33 -0400

    here's some awesome code

--- index.js
+++ index.js
@@ -7,12 +7,43 @@ function ask(questionText) {
   });
 }
 
-start();
+function getMedian(maximum, minimum) {
+  return Math.round((maximum + minimum) / 2)
+}
 
 async function start() {
+
+  let max = 100
+  let min = 1
+
+  let guess = getMedian(max, min)
+
+
+
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
-  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
+  let secretNumber = await ask("What is your secret number?");
   console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
-  process.exit();
+
+  let yesNo = await ask("Is your number" + guess)
+
+  while (yesNo !== 'y') {
+
+    if (guess < secretNumber) {
+      min = guess + 1
+      guess = getMedian(min, max)
+      yesNo = await ask ("Is your secret number the" + guess)
+
+    }
+
+    if (guess > secretNumber) {
+      max = guess
+      guess = getMedian(min, max)
+      yesNo = await ask ("Is your secret number the" + guess)
+
+    }
+
+  }
+
+  process.exit()
 }
+start()
